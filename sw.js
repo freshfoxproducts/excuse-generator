a3b9e843917be665cdb3323ac637d4e8eb3cc450
freshfoxproducts <stevecandlish@icloud.com>
2025-12-10 11:43:53 -0500

    Update manifest and PWA files

---
+++ sw.js
@@ -0,0 +1,24 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("excuse-generator-v1").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./script.js",
+        "./manifest.json",
+        "./icons/favicon.png",
+        "./icons/icon-192.png",
+        "./icons/icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
